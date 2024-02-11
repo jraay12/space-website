65e026bd3037a3fd6d7b2ba6aa891010610e54e3
John Ray <johnraycaete@gmail.com>
2024-02-11 23:52:57 +0800

    set up colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "hsl(230, 35%, 7%)",
+        white: "hsl(0, 0%, 100%)",
+        lightWhite: "hsl(231, 77%, 90%)",
+      },
+      fontFamily: {
+        bellefair: ["Bellefair", "serif"],
+      },
+    },
   },
   plugins: [],
 };
