223c49ebf7bdd0d84eca8a7213cbc4535311499a
John Ray <johnraycaete@gmail.com>
2024-02-12 12:50:18 +0800

    add configuration in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
       fontFamily: {
         bellefair: ["Bellefair", "serif"],
       },
+      backgroundImage: {
+        desktop: "url('/src/images/home/desktop.jpg')",
+        mobile: "url('/src/images/home/mobile.jpg')",
+        tablet: "url('/src/images/home/tablet.jpg')",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1024px",
+        tablet: "768px",
+      },
     },
   },
   plugins: [],
