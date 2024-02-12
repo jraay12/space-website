9e90b4f20bce034c4a66cb6f94ee4fe38b2db785
John Ray <johnraycaete@gmail.com>
2024-02-12 23:40:04 +0800

    add animation partially

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Header from "../Components/Header";
 import moon from "../images/destination/image-moon.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export default function Destination() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="min-w-full min-h-screen mobile:bg-destinationMobile tablet:bg-destinationTablet desktop:bg-destinationDesktop bg-no-repeat bg-cover overflow-hidden ">
       <section className="desktop:pt-10 pl-10 ">
@@ -13,11 +21,13 @@ export default function Destination() {
               <span className="text-gray-700 font-bold mx-4">01</span>PICK YOUR
               DESTINATION
             </h1>
-            <div className="flex items-center justify-center w-full h-full ">
-              <img
-                src={moon}
-                className="w-1/2 mt-20 object-cover "
-              />
+            <div
+              className="flex items-center justify-center w-full h-full "
+              data-aos="fade-right"
+              data-aos-duration="1500"
+              data-aos-easing="ease-in"
+            >
+              <img src={moon} className="w-1/2 mt-20 object-cover " />
             </div>
           </div>
         </section>
@@ -29,15 +39,21 @@ export default function Destination() {
               <li className="hover:text-white hover:cursor-pointer">EUROPA</li>
               <li className="hover:text-white hover:cursor-pointer">TITAN</li>
             </ul>
-            <h2 className="text-[100px]">MOON</h2>
-            <div className="w-full h-full flex justify-center desktop:justify-start">
-              {" "}
-              <p className="text-lightWhite  tablet:max-w-lg   desktop:text-start mx-10 desktop:mx-0 ">
-                See our planet as you’ve never seen it before. A perfect
-                relaxing trip away to help regain perspective and come back
-                refreshed. While you’re there, take in some history by visiting
-                the Luna 2 and Apollo 11 landing sites.
-              </p>
+            <div
+              data-aos="fade-left"
+              data-aos-duration="1500"
+              data-aos-easing="ease-in"
+            >
+              <h2 className="text-[100px]">MOON</h2>
+              <div className="w-full h-full flex justify-center desktop:justify-start">
+                {" "}
+                <p className="text-lightWhite  tablet:max-w-lg   desktop:text-start mx-10 desktop:mx-0 ">
+                  See our planet as you’ve never seen it before. A perfect
+                  relaxing trip away to help regain perspective and come back
+                  refreshed. While you’re there, take in some history by
+                  visiting the Luna 2 and Apollo 11 landing sites.
+                </p>
+              </div>
             </div>
             <div className="text-center border opacity-15 border-lightWhite mt-6"></div>
             <div className="flex flex-col tablet:flex-row tablet:justify-center desktop:justify-start mt-7 desktop:mt-4 gap-8 desktop:gap-16 mb-10 desktop:mb-0">
