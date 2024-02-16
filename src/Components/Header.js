4b6ecdaf03ec8888819a28b397388dd3a2522819
John Ray <johnraycaete@gmail.com>
2024-02-16 23:45:02 +0800

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,10 +35,16 @@ export default function Header() {
   };
   return (
     <header className="flex items-center justify-between w-full h-16 font-bellefair  pl-2 ">
-      <button className="text-white text-2xl " onClick={handleOpen}>
+      <button
+        className="tablet:hidden text-white text-2xl "
+        onClick={handleOpen}
+      >
         &#9776;
       </button>
-      <img src={Logo} className="h-[2rem] w-[2rem] order-last -translate-x-3" />
+      <img
+        src={Logo}
+        className="h-[2rem] w-[2rem] order-last tablet:order-first -translate-x-3"
+      />
       <div className=" flex-grow  ml-20 z-20 desktop:border-[1px] border-white1 brightness-50  "></div>
       <section className="mobile:hidden tablet:flex justify-end px-4 items-center  max-w-2xl h-full backdrop-blur-xl  ">
         <ul className="mobile:hidden tablet:flex text-[16px] gap-10 justify-center items-center text-white backdrop-blur-lg h-full backdrop-brightness-110  ">
